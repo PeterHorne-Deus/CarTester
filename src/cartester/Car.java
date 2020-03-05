94e13370187d46f3a2ed6ba42d249857bb8d0c82
pehor9164 <pehor9164@7HMN6Y1.Student.UGDSB.ED>
2020-03-05 13:20:35 -0500

    Commented and working!

--- src/cartester/Car.java
+++ src/cartester/Car.java
@@ -1,8 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Horne-Deus
+ * This program shows car options to a user
+ * Palindrome.java
+ * March 5, 2020
  */
+
 
 package cartester;
 
@@ -12,4 +14,51 @@ package cartester;
  */
 public class Car {
     
+    //One of the types of the cars that can be made
+    public Car(){
+        //Variables
+        int year = 1977;
+        int price = 5000000;
+        String modle = "Impulla";
+        String make = "Chirstler";
+        Boolean speakers = true;
+        Boolean table = true;
+        
+        //Outputing all inputed information for the user
+        System.out.println("Year: " + year + "\nPrice: " + price + "\nModel: " 
+                + modle + "\nMake: " + make);
+        if (speakers == true){
+            System.out.println("Speakers: YES");
+        }
+        else{
+            System.out.println("Speakers: NO");
+        }
+        
+        if (table == true){
+            System.out.println("Table: YES");
+        }
+        else{
+            System.out.println("Table: NO");
+        }
+    }
+    //Another class for a different car input
+    public Car(int year, int price, String model, String make,Boolean speaker,Boolean table){
+        //Setting the output of the car from the users info
+        System.out.println("Year: " + year + "\nPrice: " + price + "\nModel: " 
+                + model + "\nMake: " + make);
+        if (speaker == true){
+            System.out.println("Speakers: YES");
+        }
+        else{
+            System.out.println("Speakers: NO");
+        }
+        
+        if (table == true){
+            System.out.println("Table: YES");
+        }
+        else{
+            System.out.println("Table: NO");
+        }
 }
+}
+
--- src/cartester/CarTester.java
+++ src/cartester/CarTester.java
@@ -1,8 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Horne-Deus
+ * This program shows car options to a user
+ * Palindrome.java
+ * March 5, 2020
  */
+
 
 package cartester;
 
@@ -19,70 +21,74 @@ public class CarTester {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Car c;
          
-         int year, price,tables,speaker;
-         String modle,make;
-         Boolean speakers, table;
-         
-         
-         int choice = Integer.parseInt(JOptionPane.showInputDialog("Choose from the list: \n" + 
-                 "1. Get the defult car \n2. Custom car \n3. Random Hondla Civics"));
-         
-         if(choice == 1){
-            year = 1977;
-            price = 5000000;
-            modle = "Impulla";
-            make = "Chirstler";
-            speakers = true;
-            table = true;
-         }
-         else if (choice == 2){
-            year = Integer.parseInt(JOptionPane.showInputDialog("Enter the year you would like"));
-            price = (int) (Math.random() * 100000 + 500);
-            modle = JOptionPane.showInputDialog("What is the modle of the car");
-            make = JOptionPane.showInputDialog("What is the make of the car");
-            speaker = Integer.parseInt(JOptionPane.showInputDialog("Would you like a stereo?" +
-                    "1. Yes \n2. No"));
-            tables = Integer.parseInt(JOptionPane.showInputDialog("Would you like a solid oak table?" +
-                    "1. Yes \n2. No"));
-            
-            if(speaker == 1){
-                speakers = true;
-            }
-            else{
-              speakers = false;  
-            }
-            if(tables == 1){
-                table = true;
-            }
-            else{
-                table = false;
-            }
-            
-         }
-         else if(choice == 3){
-            year = (int) (Math.random() * 2010 + 1990);
-            price = (int) (Math.random() * 100000 + 500);
-            modle = "Civics";
-            make = "Hondla";
-            speaker = (int) (Math.random() * 2 + 1);
-            tables = (int) (Math.random() * 2 + 1);
-            
-            if(speaker == 1){
-                speakers = true;
-            }
-            else{
-              speakers = false;  
-            }
-            if(tables == 1){
-                table = true;
-            }
-            else{
-                table = false;
-            }
-             System.out.println(year + " "+ price + " " + modle + " " + make  + " " + speakers + " " + table);
-         }
+        //Variables and decliration
+        Car c; 
+        int year, price,tables,speaker;
+        String modle,make;
+        Boolean speakers, table;
+
+        //Getting user input of how they would like to procced
+        int choice = Integer.parseInt(JOptionPane.showInputDialog("Choose from the list: \n" + 
+                "1. Get the defult car \n2. Custom car \n3. Random Hondla Civics"));
+
+        //Changing code depending on user input
+        if(choice == 1){
+           c = new Car();
+
+        }
+        //Making the car with the users choice
+        else if (choice == 2){
+           //Setting all of the car variables based on user input for a custom car
+           year = Integer.parseInt(JOptionPane.showInputDialog("Enter the year you would like"));
+           price = (int) (Math.random() * 100000 + 500);
+           modle = JOptionPane.showInputDialog("What is the modle of the car");
+           make = JOptionPane.showInputDialog("What is the make of the car");
+           speaker = Integer.parseInt(JOptionPane.showInputDialog("Would you like a stereo?" +
+                   "\n1. Yes \n2. No"));
+           tables = Integer.parseInt(JOptionPane.showInputDialog("Would you like a solid oak table?" +
+                   "\n1. Yes \n2. No"));
+
+           if(speaker == 1){
+               speakers = true;
+           }
+           else{
+             speakers = false;  
+           }
+           if(tables == 1){
+               table = true;
+           }
+           else{
+               table = false;
+           }
+           //Sending input to a method in the Car class
+           c = new Car(year,price,modle,make,speakers,table);
+        }
+        //Making the car with the users input
+        else if(choice == 3){
+           //Setting random information to a predermined car for the user
+           year = (int) (Math.random() * 2010 + 1990);
+           price = (int) (Math.random() * 100000 + 500);
+           modle = "Civics";
+           make = "Hondla";
+           speaker = (int) (Math.random() * 2 + 1);
+           tables = (int) (Math.random() * 2 + 1);
+
+           if(speaker == 1){
+               speakers = true;
+           }
+           else{
+             speakers = false;  
+           }
+           if(tables == 1){
+               table = true;
+           }
+           else{
+               table = false;
+           }
+           //Sending the car info to the car method in the car class
+           c = new Car(year,price,modle,make,speakers,table);
+        }
          
          
          
